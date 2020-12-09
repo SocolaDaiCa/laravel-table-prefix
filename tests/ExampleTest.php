3bc4a6a2e80a600d87983389ef87b8abad2762ba
Socola Đại Ca <tokentien@gmail.com>
2020-12-09 01:45:18 +0000

    update .phpunit.result.cach

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,15 +3,14 @@
 namespace SocolaDaiCa\LaravelTablePrefix\Tests;
 
 use Orchestra\Testbench\TestCase;
-use SocolaDaiCa\LaravelTablePrefix\LaravelTablePrefixServiceProvider;
 
 class ExampleTest extends TestCase
 {
 
-    protected function getPackageProviders($app)
-    {
-        return [LaravelTablePrefixServiceProvider::class];
-    }
+    // protected function getPackageProviders($app)
+    // {
+    //     return [LaravelTablePrefixServiceProvider::class];
+    // }
     
     /** @test */
     public function true_is_true()
